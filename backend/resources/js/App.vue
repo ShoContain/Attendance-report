a6c900d8fc44989f7b48c1e5f31e57150f01397c
ShoContain <perblue.666@gmail.com>
2021-12-04 12:41:07 +0900

    add: vue3 & vue-routerの導入

---
+++ backend/resources/js/App.vue
@@ -0,0 +1,6 @@
+<script setup>
+   import ExapmpleComponent from './components/ExampleComponent.vue'
+</script>
+<template>
+    <ExapmpleComponent title="Hoge"/>
+</template>
---
+++ backend/resources/js/components/ExampleComponent.vue
@@ -0,0 +1,15 @@
+<script setup>
+    const props = defineProps({
+        title: {
+          type: String,
+          default: '',
+          required: true
+        }
+      })
+</script>
+
+<template>
+    <div class="container">
+        {{ props.title }}
+    </div>
+</template>
