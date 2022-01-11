ea07471c28418c511ccef40336e579291848c492
田中 翔太 <perblue.666@gmail.com>
2022-01-11 16:14:51 +0900

    add: API呼び出しの雛形を作成

--- backend/resources/js/components/FullScreenSection.vue
+++ backend/resources/js/components/FullScreenSection.vue
@@ -1,6 +1,6 @@
 <template>
     <section
-    class="px-0 md:px-6 flex w-full h-screen items-center justify-center bg-slate-800"
+    class="px-0 md:px-6 flex w-full h-screen items-center justify-center bg-slate-600"
   >
     <slot name="content" />
   </section>
--- backend/resources/js/pages/Login.vue
+++ backend/resources/js/pages/Login.vue
@@ -1,33 +1,49 @@
 <script setup >
+import { reactive } from 'vue'
+import { login } from '@/api/user'
 
+const form = reactive({
+  login: '',
+  password:'',
+  remember:false
+})
+
+const handleLogin = async() => {
+  try{
+    const data = await login('ditto')
+    console.log(data)
+  }catch(err){
+    console.log(err)
+  }
+}
 </script>
 
 <template>
-  <form class="bg-white shadow-md rounded p-8 mb-4">
+  <form class="bg-white shadow-md rounded p-8 mb-2" @submit.prevent="handleLogin">
   <div class="md:flex md:items-center mb-6">
     <div class="md:w-1/3">
-      <label class="block text-gray-500 font-bold md:text-right mb-1 md:mb-0 pr-4" for="inline-full-name">
-        ログインID
+      <label class="block text-gray-500 text-sm font-bold md:text-right mb-1 md:mb-0 pr-4" for="inline-full-name">
+        ID
       </label>
     </div>
     <div class="md:w-2/3">
-      <input class="bg-gray-200 appearance-none border-2 border-gray-200 rounded w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-purple-500" id="inline-full-name" type="text" value="Jane">
+      <input class="bg-gray-200 appearance-none border-2 border-gray-200 rounded w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-purple-500" id="inline-full-name" type="text" v-model = "form.login">
     </div>
   </div>
   <div class="md:flex md:items-center mb-6">
     <div class="md:w-1/3">
-      <label class="block text-gray-500 font-bold md:text-right mb-1 md:mb-0 pr-4" for="inline-password">
+      <label class="block text-gray-500 text-sm font-bold md:text-right mb-1 md:mb-0 pr-4" for="inline-password">
         パスワード
       </label>
     </div>
     <div class="md:w-2/3">
-      <input class="bg-gray-200 appearance-none border-2 border-gray-200 rounded w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-purple-500" id="inline-password" type="password" placeholder="******************">
+      <input class="bg-gray-200 appearance-none border-2 border-gray-200 rounded w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-purple-500" id="inline-password" type="password" v-model="form.password" placeholder="******************">
     </div>
   </div>
   <div class="md:flex md:items-center mb-6">
     <div class="md:w-1/3"></div>
     <label class="md:w-2/3 block text-gray-500 font-bold">
-      <input class="mr-2 leading-tight" type="checkbox">
+      <input class="mr-2 leading-tight" type="checkbox" v-model="form.remember">
       <span class="text-sm">
         ログイン情報を記憶する
       </span>
@@ -36,7 +52,7 @@
   <div class="md:flex md:items-center">
     <div class="md:w-1/3"></div>
     <div class="md:w-1/3">
-      <button class="shadow bg-slate-700 hover:bg-slate-600 focus:shadow-outline focus:outline-none text-white font-bold py-2 px-4 rounded" type="button">
+      <button type="submit" class="shadow bg-slate-700 hover:bg-slate-600 focus:shadow-outline focus:outline-none text-white font-bold py-2 px-4 rounded">
         ログイン
       </button>
     </div>
