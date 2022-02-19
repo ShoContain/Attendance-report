c13435693a6508f08cf8ee62ea32e4f52ca0bd01
ShoContain <perblue.666@gmail.com>
2022-02-19 14:14:12 +0900

    add: Element-plusの設定をapp.jsに追加

--- backend/resources/js/app.js
+++ backend/resources/js/app.js
@@ -8,5 +8,8 @@ require("./bootstrap")
 import { createApp } from "vue"
 import router from "./router"
 import App from "./App.vue"
+import ElementPlus from 'element-plus';
+import 'element-plus/dist/index.css'
+import locale from "element-plus/lib/locale/lang/en";
 
-createApp(App).use(router).mount("#app")
+createApp(App).use(router).use(ElementPlus,{locale}).mount("#app")
