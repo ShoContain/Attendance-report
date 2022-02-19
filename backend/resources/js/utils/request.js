6cac32e0aa0991ed549bf5b8b8c808009d4c8646
ShoContain <perblue.666@gmail.com>
2022-02-19 14:15:55 +0900

    add: リスポンドのエラーメッセージ表示を共通化

--- backend/resources/js/utils/request.js
+++ backend/resources/js/utils/request.js
@@ -1,4 +1,5 @@
 import axios from "axios"
+import { ElMessage } from 'element-plus'
 
 // create an axios instance
 const service = axios.create({
@@ -7,4 +8,29 @@ const service = axios.create({
   timeout: 5000, // request timeout
 })
 
+service.interceptors.response.use(
+  response => {
+    // if (response.headers.authorization) {
+    //   setLogged(response.headers.authorization);
+    //   response.data.token = response.headers.authorization;
+    // }
+
+    return response.data;
+  },
+  error => {
+    let message = error.message;
+    if (error.response.data && error.response.data.errors) {
+      message = error.response.data.errors;
+    } else if (error.response.data && error.response.data.error) {
+      message = error.response.data.error;
+    }
+
+    ElMessage({
+      message: message,
+      type: 'error',
+      duration: 5 * 1000,
+    });
+    return Promise.reject(error);
+  }
+);
 export default service
