67847543c05a929ac4af2f6cfcd9c10a6b19715e
田中　翔太 <perblue.666@gmail.com>
2022-03-08 18:05:56 +0900

    add: カテゴリーテーブル作成とカテゴリーと映画の紐付け

--- backend/app/Models/Movie.php
+++ backend/app/Models/Movie.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 
 class Movie extends Model
 {
@@ -18,5 +19,10 @@ class Movie extends Model
     public function scheduledMovies()
     {
         return $this->hasMany('App\Models\scheduledMovie');
+    }
+
+    public function categories(): BelongsToMany
+    {
+        return $this->belongsToMany(Category::class);
     }
 }
